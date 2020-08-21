b24c284f758389f99f7b710ac488362cce1261aa
Divyashree <divyashreedwivedi@icloud.com>
2020-08-22 00:06:49 +0530

    CommandLine Application With SpringBoot

---
+++ src/main/java/com/dwiveddi/boot/RoomCleaningPrimer.java
@@ -0,0 +1,25 @@
+package com.dwiveddi.boot;
+
+import org.springframework.stereotype.Component;
+import java.util.Arrays;
+import java.util.List;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.web.client.RestTemplate;
+
+
+@Component
+public class RoomCleaningPrimer implements CommandLineRunner {
+    private RestTemplate restTemplate;
+
+    public RoomCleaningPrimer (){
+        super();
+        this.restTemplate = new RestTemplate();
+    }
+    @Override
+    public void run(String... strings) throws Exception {
+        String url = "http://localhost:8080/api/rooms";
+        Room[] roomArray = this.restTemplate.getForObject(url, Room[].class);
+        List<Room> rooms = Arrays.asList(roomArray);
+        rooms.forEach(System.out::println);
+    }
+}
---
+++ src/test/java/com/dwiveddi/boot/RoomClrAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dwiveddi.boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoomClrAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
